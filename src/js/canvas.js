f0de5aa9a81698e98baf3a8f12fb685bc1c7a0ba
Amandine Donat-Filliod <amandine.donat.filliod@gmail.com>
2019-02-06 13:47:49 +0100

    màj 06.02.19

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,146 +1,172 @@
-window.onload = function () {
+window.onload = function() {
+  //---- SCRIPT ----//
 
-    // LIBRARIES
-    var THREE = require('three');
+  // LOADING STYLE
+  var loader = document.getElementById("loader");
+  setTimeout(function() {
+    loader.style.opacity = 0;
+  }, 1000);
 
-    // IMAGES
-    var planetImage = require('../images/texture/sky.png');
-    var skyImage = require('../images/texture/sky.png');
-    var satelliteImage = require('../images/texture/planet2.jpg');
-    var floorImage = require('../images/texture/grid.svg');
+  // TRANSITION
+  var enterButton = document.getElementById("enterButton");
+  enterButton.addEventListener("click", function(e) {
+    e.preventDefault();
 
-    //--------//
+    loader.style.opacity = 1;
 
-    // SCENE
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 1000 );
-    camera.position.z = 10;
+    setTimeout(function() {
+      var destination = enterButton.getAttribute("href");
+      window.location = destination;
+    }, 500);
+  });
 
-    var renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize( window.innerWidth, window.innerHeight, /*updateStyle*/ );
-    document.body.appendChild( renderer.domElement ); //create a <canvas> element
+  //---- WEBGL ----//
 
-    let canvas = document.querySelector('canvas');
+  // LIBRARIES
+  var THREE = require("three");
+
+  // IMAGES & TEXTURES
+  var skyImage = require("../images/texture/sky.png");
+  var satelliteImage = require("../images/texture/planet.jpg");
+
+  var skyTexture = new THREE.TextureLoader().load(skyImage);
+  var satelliteTexture = new THREE.TextureLoader().load(satelliteImage);
+
+  // SCENE
+  var scene = new THREE.Scene();
+
+  var camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.z = 10;
+
+  var renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight /*updateStyle*/);
+  document.body.appendChild(renderer.domElement); //create a <canvas> element
+
+  let canvas = document.querySelector("canvas");
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  window.onresize = function() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    
-    window.onresize = function(){
-       canvas.height = window.innerHeight;
-       canvas.width = window.innerWidth;
-    }
+  };
 
-    //LIGHTS
+  //LIGHTS
 
-    var light = new THREE.PointLight( 0xffffff, 2, 100 );
-    light.position.set( 0, 15, 25 );
-    scene.add( light );
+  var light = new THREE.PointLight(0xffffff, 3, 100);
+  light.position.set(0, 15, 25);
+  scene.add(light);
 
-    /*var light2 = new THREE.PointLight( 0xffe7a8, 0.1, 100 );
+  /*var light2 = new THREE.PointLight( 0xffe7a8, 0.1, 100 );
     light2.position.set( 0, -20, -20 );
-    scene.add( light2 );
-    */
+    scene.add( light2 );*/
 
-    var ambientLight = new THREE.AmbientLight( 0x909090 );
-    scene.add( ambientLight );
+  var ambientLight = new THREE.AmbientLight(0xdedede);
+  scene.add(ambientLight);
 
-    //SKYBOX
+  // PLANE
 
-    var skyGeometry = new THREE.SphereGeometry(90, 50, 50);
+  var geometry = new THREE.PlaneGeometry( 125, 60, 32 );
 
-    var skyTexture = new THREE.TextureLoader().load(skyImage);
-    var skyMaterial = new THREE.MeshPhongMaterial({
-        map: skyTexture,
-        //color: 0x000000,
-        side: THREE.DoubleSide,
-        shininess: 0
-    })
+  var material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    map: skyTexture,
+    side: THREE.DoubleSide
+  });
+  var plane = new THREE.Mesh( geometry, material );
+  plane.position.set(0, 0, -60);
+  scene.add( plane );
 
-    var skyBox = new THREE.Mesh(skyGeometry, skyMaterial);
-    scene.add(skyBox);
+  //SATELLITE
 
-    // PLANE
+  var satelliteGeometry = new THREE.SphereGeometry(0.3, 50, 50);
 
-    /*var floorGeometry = new THREE.PlaneGeometry( 12, 5, 20);
+  var satelliteMaterial = new THREE.MeshStandardMaterial({
+    map: satelliteTexture,
+    color: 0xaaaaaa,
+    roughness: 0.7,
+    metalness: 0.4
+  });
 
-    var floorTexture = new THREE.TextureLoader().load(floorImage);
-    var floorMaterial = new THREE.MeshBasicMaterial({
-        map: floorTexture,
-        side: THREE.DoubleSide
-    });
+  var satellite = new THREE.Mesh(satelliteGeometry, satelliteMaterial);
+  satellite.position.set(6, 0, 0);
+  scene.add(satellite);
 
-    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    //floor.position.y = -4;
-    //floor.rotation.x = Math.PI / 1.8;
-    scene.add( floor );*/
+  //BIG PLANET
 
+  var planetGeometry = new THREE.SphereGeometry(2.8, 50, 50); //object that contains all the points (vertices) and faces
 
-    //SATELLITE
+  var planetMaterial = new THREE.MeshStandardMaterial({
+    map: skyTexture,
+    color: 0xaaaaaa,
+    roughness: 0.7,
+    metalness: 0.4
+  });
 
-    var satelliteGeometry = new THREE.SphereGeometry(0.3, 50, 50);
+  var planet = new THREE.Mesh(planetGeometry, planetMaterial);
+  planet.rotation.z += 90;
+  scene.add(planet);
 
-    var satelliteTexture = new THREE.TextureLoader().load(satelliteImage);
-    var satelliteMaterial = new THREE.MeshStandardMaterial({
-        map: satelliteTexture,
-        color: 0xaaaaaa,
-        roughness : 0.7,
-        metalness : 0.4
-    });
+  // GRID
+  var horizontalGrid = new THREE.GridHelper(100, 20);
+  horizontalGrid.position.set(0, -4, 0);
+  scene.add(horizontalGrid);
 
-    var satellite = new THREE.Mesh(satelliteGeometry, satelliteMaterial);
-    satellite.position.set(6, 0, 0);
-    scene.add(satellite);
+  var verticalGrid = new THREE.GridHelper(91, 20);
+  //verticalGrid.position.set(0, 28.8, -9.8);
+  verticalGrid.position.set(0, 42, -43);
+  verticalGrid.rotation.x = Math.PI / 2;
+  scene.add(verticalGrid);
 
-    //BIG PLANET
+  //MOUSE MOVE
 
-    var planetGeometry = new THREE.SphereGeometry( 2.8, 50, 50 ); //object that contains all the points (vertices) and faces
+  var mouse = { x: 0, y: 0 };
+  var cameraMoves = { x: 0, y: 0, z: -0.1, move: false, speed: 0.2 };
 
-    var planetTexture = new THREE.TextureLoader().load(planetImage);
-    var planetMaterial = new THREE.MeshStandardMaterial({
-        map : planetTexture,
-        color: 0xaaaaaa,
-        roughness : 0.7,
-        metalness : 0.4
-    });
+  function mouseMove(e) {
+    camera.position.x += Math.max(
+      Math.min((e.clientX - mouse.x) * 0.0004, cameraMoves.speed),
+      -cameraMoves.speed
+    );
+    camera.position.y += Math.max(
+      Math.min((mouse.y - e.clientY) * 0.0004, cameraMoves.speed),
+      -cameraMoves.speed
+    );
 
-    var planet = new THREE.Mesh( planetGeometry, planetMaterial );
-    planet.rotation.z += 90;
-    scene.add( planet );
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+  }
+  window.addEventListener("mousemove", mouseMove);
 
-    // GRID
-    var horizontalGrid = new THREE.GridHelper( 120, 35 );
-    horizontalGrid.position.set(0, -4, 0);
-    scene.add(horizontalGrid);
+  //RENDERING THE SCENE : CREATING AN ANIMATION LOOP EVERY TIME THE SCREEN IS REFRESHED (60fps)
 
-    var verticalGrid = new THREE.GridHelper( 60, 20);
-    verticalGrid.position.set(0, 28.8, -9.8);
-    verticalGrid.rotation.x = Math.PI / 2;
-    scene.add(verticalGrid);
+  var clock = new THREE.Clock();
+  var r = 5;
+  var counter = 0;
+  var incrementation = (2 * Math.PI) / 1000; //TODO : convert in degrees
 
-    //RENDERING THE SCENE : CREATING AN ANIMATION LOOP EVERY TIME THE SCREEN IS REFRESHED (60fps)
+  var render = function() {
+    var delta = clock.getDelta(); //get time between each frames (always stored in var)
 
-    var clock = new THREE.Clock();
-    var r = 5;
-    var counter = 0;
-    var incrementation = 2 * Math.PI / 1000; //TODO : convert in degrees
+    //planet rotation animation
+    planet.rotation.x -= (Math.PI / 180) * 10 * delta; //convert radiant (initial) to degrees -> 10° per second
 
-    var render = function () {
-        var delta = clock.getDelta(); //get time between each frames (always stored in var)
+    //satellite rotation animation
+    //satellite.rotation.y -= Math.PI / 180*10 * delta;
 
-        //planet rotation animation
-        planet.rotation.x -= Math.PI / 180*10 * delta; //convert radiant (initial) to degrees -> 10° per second
+    //orbit animation
+    counter += incrementation;
+    satellite.position.x = r * Math.cos(counter);
+    satellite.position.z = r * Math.sin(counter);
 
-        //satellite rotation animation
-        //satellite.rotation.y -= Math.PI / 180*10 * delta;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render); //60fps
+  };
 
-        //orbit animation
-        counter += incrementation;
-        satellite.position.x = r * Math.cos(counter);
-        satellite.position.z = r * Math.sin(counter);
-
-        renderer.render( scene, camera );
-        requestAnimationFrame( render ); //60fps
-    };
-
-    render();
-
-}
+  render();
+};
--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,81 @@
-//     SIN WAVE     //
 window.onload = function() {
-    let canvas = document.querySelector('.wave');
-    let ctx = canvas.getContext('2d');
+  //---- SCRIPT ----//
+  var loader = document.getElementById("loader");
+  setTimeout(function() {
+    loader.style.opacity = 0;
+  }, 500);
+
+  //---- SIN WAVE ----//
+  let canvas = document.querySelector(".wave");
+  let ctx = canvas.getContext("2d");
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  window.onresize = function() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    
-    window.onresize = function(){
-       canvas.height = window.innerHeight;
-       canvas.width = window.innerWidth;
-    }
-    
-    var counter = 0;
-    var deltaTime = 0;
-    var lastTime = Date.now();
+  };
 
-    function update() {
-        //Delta Time
-      deltaTime = (Date.now() - lastTime)/1000;
-      lastTime = Date.now();
-      
-      ctx.clearRect( 0, 0, window.innerWidth, window.innerHeight);
-      
-      //Animation
-      counter += 0.08 * Math.PI * 2 * deltaTime; //percent(animation speed) * one sin * per second
-      
-      //Draw
-      ctx.beginPath();
-      ctx.moveTo(0, window.innerHeight);
-      
-      var ratio = window.innerWidth/1920;
-      
-      ctx.strokeStyle = "white";
-      ctx.lineWidth = 350 * ratio;
-      
-      var precision = 15;
-      var amplitude = 100 * ratio;
-      
-      for(let i = 0; i <= precision; i++) {
-        let sin = Math.sin(counter + (i / precision) * Math.PI * 2 );
-        let x = (window.innerWidth / precision) * i  + sin  * amplitude;
-        let y = (window.innerHeight / precision) * (precision - i) + sin * amplitude;
-        
-        ctx.lineTo(x, y);
-      }
+  var counter = 0;
+  var deltaTime = 0;
+  var lastTime = Date.now();
 
-      ctx.globalAlpha = 0.05; //Note : globalAlpha is for all of the canvas
-      ctx.stroke();
-      ctx.filter = "blur(20px)";
-      
-      requestAnimationFrame( update );
+  function update() {
+    //Delta Time
+    deltaTime = (Date.now() - lastTime) / 1000;
+    lastTime = Date.now();
+
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    //Animation
+    counter += 0.08 * Math.PI * 2 * deltaTime; //percent(animation speed) * one sin * per second
+
+    //Draw
+    ctx.beginPath();
+    ctx.moveTo(0, window.innerHeight);
+
+    var ratio = window.innerWidth / 1920;
+
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 350 * ratio;
+
+    var precision = 15;
+    var amplitude = 100 * ratio;
+
+    for (let i = 0; i <= precision; i++) {
+      let sin = Math.sin(counter + (i / precision) * Math.PI * 2);
+      let x = (window.innerWidth / precision) * i + sin * amplitude;
+      let y =
+        (window.innerHeight / precision) * (precision - i) + sin * amplitude;
+
+      ctx.lineTo(x, y);
     }
-    
-    update();
+
+    ctx.globalAlpha = 0.05; //Note : globalAlpha is for all of the canvas
+    ctx.stroke();
+    ctx.filter = "blur(20px)";
+
+    requestAnimationFrame(update);
   }
+
+  update();
+
+  //---- PARTICLES ----//
+  setInterval(function() {
+    var particle = document.createElement("div");
+    particle.style.top = window.innerHeight * Math.random() + "px";
+    particle.style.left = window.innerWidth * Math.random() + "px";
+
+    var span = document.createElement("span");
+    span.innerHTML = Math.random() > 0.5 ? "◯" : "+";
+    span.style.fontSize = 10 * Math.random() + 20 + "px";
+
+    var particles = document.getElementById("particles");
+    particle.append(span);
+    particles.append(particle);
+
+    setTimeout(function() {
+      particle.remove();
+    }, 10000);
+  }, 1000);
+};
